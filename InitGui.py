2d6dd84efbeaff034c37009a36ad005b08c8e080
DanielPerka <130476778+DanielPerka@users.noreply.github.com>
2023-05-15 16:51:03 +0200

    Add files via upload

---
+++ InitGui.py
@@ -0,0 +1,72 @@
+from PySide import QtCore
+import FreeCADGui as Gui
+
+import os
+
+#__dirname__ = C:\Users\DanielP\AppData\Roaming\FreeCAD\Mod\ExampleWorkbench
+
+
+
+class MyExampleWorkbench (Workbench):
+
+    MenuText = "My Workbench"
+    ToolTip = "This is my example Workbench"
+    Icon = """
+			/* XPM */
+			static const char *test_icon[]={
+			"16 16 2 1",
+			"a c #000000",
+			". c None",
+			"................",
+			"................",
+			"..############..",
+			"..############..",
+			"..############..",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"................",
+			"................"};
+			"""
+	
+	
+	
+	
+	
+	#os.path.join(__dirname__, 'icon', 'ExampleLogo.svg')
+	
+	
+
+    def Initialize(self):
+        """This function is executed when FreeCAD starts"""
+        import MyCommands#, MyModuleB # import here all the needed files that create your FreeCAD commands
+        self.list = ["MyCommand1", "MyCommand2"] # A list of command names created in the line above
+        self.appendToolbar("My Commands",self.list) # creates a new toolbar with your commands
+        self.appendMenu("Example Menu",self.list) # creates a new menu
+        self.appendMenu(["An existing Menu","My submenu"],self.list) # appends a submenu to an existing menu
+
+    def Activated(self):
+        """This function is executed when the workbench is activated"""
+        return
+
+    def Deactivated(self):
+        """This function is executed when the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This is executed whenever the user right-clicks on screen"""
+        # "recipient" will be either "view" or "tree"
+        self.appendContextMenu("My commands",self.list) # add commands to the context menu
+
+    def GetClassName(self): 
+        # This function is mandatory if this is a full python workbench
+        # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
+        return "Gui::PythonWorkbench"
+       
+Gui.addWorkbench(MyExampleWorkbench())
---
+++ MyCommands.py
@@ -0,0 +1,66 @@
+import FreeCADGui
+import FreeCAD
+from random import *
+#from PySide.QtGui import QWidget, QLabel, QHBoxLayout, QPushButton, QFont
+import Mesh
+
+class Function1_Class():
+    """My new command"""
+
+    def GetResources(self):
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/MyWorkbench/icon/test-icon.svg'
+        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
+                'Accel' : "Shift+M", # a default shortcut (optional)
+                'MenuText': "My New Command 1",
+                'ToolTip' : "What my new command 1 does"}
+
+    def Activated(self):
+        doc = FreeCAD.ActiveDocument
+        box = doc.addObject("Part::Box", "myBox")
+        box.Height = random()*100
+        box.Width = random()*100
+        box.Length = random()*100
+        print("Big box created!!")
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        if FreeCAD.ActiveDocument == None:
+            return False
+        else:
+            return True
+
+class Function2_Class():
+    """My new command"""
+
+    def GetResources(self):
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/MyWorkbench/icon/test2-icon.svg'
+        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
+                'Accel' : "Shift+J", # a default shortcut (optional)
+                'MenuText': "My New Command 2",
+                'ToolTip' : "What my new command 2 does"}
+
+    def Activated(self):
+        if FreeCAD.ActiveDocument == None:
+            doc = FreeCAD.newDocument()
+        else:
+            doc = FreeCAD.ActiveDocument
+        box = doc.addObject("Part::Box", "myBox")
+        box.Height = random()*10
+        box.Width = random()*10
+        box.Length = random()*10
+        print("Box created!!")
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+        
+        
+
+
+FreeCADGui.addCommand('MyCommand1',Function1_Class())
+FreeCADGui.addCommand('MyCommand2',Function2_Class())
+#FreeCADGui.addCommand('Exporting STL', MyGui(QWidget))
