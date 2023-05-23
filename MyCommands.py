3c2c55d8c2f080571368c78da5b8bf60246355cb
DanielPerka <130476778+DanielPerka@users.noreply.github.com>
2023-05-23 21:10:33 +0200

    Add files via upload
    Added functions: 
    - Creating directories 
    - Creating TIBRA Parameters files
    - Running TIBRA draft

--- MyCommands.py
+++ MyCommands.py
@@ -1,45 +1,23 @@
 import FreeCADGui
 import FreeCAD
 from random import *
-#from PySide.QtGui import QWidget, QLabel, QHBoxLayout, QPushButton, QFont
+from FreeCAD_PySide import *
+import StlExportManager
+import TibraParameters
+import RunTibra
 import Mesh
+import sys
 
-class Function1_Class():
-    """My new command"""
-
-    def GetResources(self):
-        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/MyWorkbench/icon/test-icon.svg'
-        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
-                'Accel' : "Shift+M", # a default shortcut (optional)
-                'MenuText': "My New Command 1",
-                'ToolTip' : "What my new command 1 does"}
-
-    def Activated(self):
-        doc = FreeCAD.ActiveDocument
-        box = doc.addObject("Part::Box", "myBox")
-        box.Height = random()*100
-        box.Width = random()*100
-        box.Length = random()*100
-        print("Big box created!!")
-        return
-
-    def IsActive(self):
-        """Here you can define if the command must be active or not (greyed) if certain conditions
-        are met or not. This function is optional."""
-        if FreeCAD.ActiveDocument == None:
-            return False
-        else:
-            return True
 
 class Function2_Class():
     """My new command"""
 
     def GetResources(self):
-        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/MyWorkbench/icon/test2-icon.svg'
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/TIBRA4FreeCAD/icon/test2-icon.svg'
         return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
                 'Accel' : "Shift+J", # a default shortcut (optional)
-                'MenuText': "My New Command 2",
-                'ToolTip' : "What my new command 2 does"}
+                'MenuText': "Create Random Box",
+                'ToolTip' : "Create Random Box"}
 
     def Activated(self):
         if FreeCAD.ActiveDocument == None:
@@ -57,10 +35,74 @@ class Function2_Class():
         """Here you can define if the command must be active or not (greyed) if certain conditions
         are met or not. This function is optional."""
         return True
+    
+class Function3_Class():
+    """My new command"""
+
+    def GetResources(self):
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/TIBRA4FreeCAD/icon/test-icon.svg'
+        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
+                'MenuText': "Export STL",
+                'ToolTip' : "Export the STL file"}
+
+    def Activated(self):
         
+        form = StlExportManager.StlExportManager()
+        form.exec_()
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        if FreeCAD.ActiveDocument == None:
+            return False
+        else:
+            return True
         
+class Function4_Class():
+    """My new command"""
+
+    def GetResources(self):
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/TIBRA4FreeCAD/icon/test-icon.svg'
+        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
+                'MenuText': "Tibra Parameters",
+                'ToolTip' : "Setting Path"}
+
+    def Activated(self):
+        
+        form = TibraParameters.TibraParameters()
+        form.exec_()
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
 
 
-FreeCADGui.addCommand('MyCommand1',Function1_Class())
-FreeCADGui.addCommand('MyCommand2',Function2_Class())
-#FreeCADGui.addCommand('Exporting STL', MyGui(QWidget))
+class RunTibra_Class():
+  
+
+    def GetResources(self):
+        icon_path = FreeCAD.getUserAppDataDir()+'/Mod/TIBRA4FreeCAD/icon/test-icon.svg'
+        return {'Pixmap'  :  str(icon_path), # the name of a svg file available in the resources
+                'MenuText': "Run Tibra",
+                'ToolTip' : "Run Tibra"}
+
+    def Activated(self):
+        
+        form = RunTibra.RunTibra()
+        form.exec_()
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+
+
+
+FreeCADGui.addCommand('Create Random Box',Function2_Class())
+FreeCADGui.addCommand('Export STL',Function3_Class())
+FreeCADGui.addCommand('Set Tibra Parameters',Function4_Class())
+FreeCADGui.addCommand('Run Tibra',RunTibra_Class())
---
+++ StlExportManager.py
@@ -0,0 +1,57 @@
+from FreeCAD_PySide import *
+import sys
+import os
+import FreeCAD
+import Mesh
+
+class StlExportManager(QtGui.QDialog):
+    """"""
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        #position and geometry of the dialog box
+        width = 230
+        height = 125
+        centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
+        self.setGeometry(centerPoint.x()-0.5*width, centerPoint.y()-0.5*height, width, height)
+        self.setWindowTitle("STL Export Manager")
+
+        #label text
+        self.label_ = QtGui.QLabel("Please enter the file name:", self)
+        self.label_.move(10, 20)
+        self.textInput_ = QtGui.QLineEdit(self)
+        self.textInput_.setText("Appear Trial")
+        self.textInput_.setFixedWidth(200)
+        self.textInput_.move(10, 50)
+
+        # cancel button
+        cancelButton = QtGui.QPushButton('Cancel', self)
+        cancelButton.clicked.connect(self.onCancel)
+        cancelButton.move(30, 90)
+        # OK button
+        okButton = QtGui.QPushButton('OK', self)
+        okButton.clicked.connect(self.onOk)
+        okButton.setAutoDefault(True)
+        okButton.move(120, 90)
+
+    def onOk(self):
+        doc = FreeCAD.ActiveDocument
+        object = []
+        object.append(FreeCAD.getDocument(FreeCAD.ActiveDocument.Name).getObject(FreeCAD.ActiveDocument.myBox.Name))
+        Mesh.export(object,os.getcwd() +"/" + "data" "/" + self.textExtractor() + ".stl")
+        infoBox = QtGui.QMessageBox.information(self, "STL Export Manager", "The STL file has been successfully exported to " \
+                                                 + "'" + os.getcwd() + "\data" + " directory.", QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+        if infoBox == QtGui.QMessageBox.StandardButton.Ok:
+            self.close()
+
+    def onCancel(self):
+        self.result = "Cancel"
+        self.close()
+    
+    def textExtractor(self):
+         self.myStr = self.textInput_.text()
+         return self.myStr
+    
