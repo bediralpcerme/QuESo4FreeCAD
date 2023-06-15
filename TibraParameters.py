b3596cc83f2bc335b7bb69ab3e8fd6f884b160b0
bediralpcerme <bediralp.cerme@tum.de>
2023-06-15 18:37:08 +0200

    BoundaryConditions v0.1

--- TibraParameters.py
+++ TibraParameters.py
@@ -1,9 +1,16 @@
 from FreeCAD_PySide import *
 import os
 import FreeCAD
+import FreeCADGui as Gui
 import Mesh
 import json
+from pivy import coin
 
+##################
+
+# The code is not finished yet
+
+##################
 
 #TO DO LIST: 
 # - Font size in pop-up windows could be greater a bit
@@ -11,26 +18,33 @@ import json
 class TibraParameters(QtGui.QDialog):
     """"""
     def __init__(self):
-        super().__init__()
+        super(TibraParameters, self).__init__()
         self.initUI()
 
     def initUI(self):
 
         #position and geometry of the dialog box
         width = 340
-        height = 600
-        centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
-        self.setGeometry(centerPoint.x()-0.5*width, centerPoint.y()-0.5*height, width, height)
+        height = 700
+        self.centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
+        self.setGeometry(self.centerPoint.x()-0.5*width, self.centerPoint.y()-0.5*height, width, height)
         self.setWindowTitle("Tibra Parameters")
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
         self.docName =  FreeCAD.ActiveDocument.Label + ".FCStd"
         self.work_dir = FreeCAD.ActiveDocument.FileName
         self.work_dir = self.work_dir.replace(self.docName,"")
+        self.Neumann_count = 1
+        self.dirichlet_count = 1
 
         #Initial Parameters input:
 
         #main (general) head
         self.label_main_ = QtGui.QLabel("General settings:", self)
         self.label_main_.move(10, 10)
+        boldFont=QtGui.QFont()
+        boldFont.setBold(True)
+        self.label_main_.setFont(boldFont)
 
         #path to the file
         self.label_pathname_ = QtGui.QLabel("Path to the STL file:", self)
@@ -60,6 +74,7 @@ class TibraParameters(QtGui.QDialog):
         #mesh head
         self.label_main_ = QtGui.QLabel("Mesh settings:", self)
         self.label_main_.move(10, 160)
+        self.label_main_.setFont(boldFont)
 
         # we dont need it anymore but im leaving it here just in case
         # #lower bound
@@ -142,6 +157,7 @@ class TibraParameters(QtGui.QDialog):
         #solution settings head
         self.label_main_ = QtGui.QLabel("Solution settings:", self)
         self.label_main_.move(10, 400)
+        self.label_main_.setFont(boldFont)
 
         #residual setting
         self.label_residual_ = QtGui.QLabel("Moment fitting residual:", self)
@@ -159,16 +175,38 @@ class TibraParameters(QtGui.QDialog):
         self.popup_integration.addItems(self.popup_integration_items)
         self.popup_integration.setFixedWidth(200)
         self.popup_integration.move(10, 490)
-        
+
+        #BC settings
+        self.label_ApplyBC_ = QtGui.QLabel("Boundary Conditions:", self)
+        self.label_ApplyBC_.move(10, 535)
+        self.label_ApplyBC_.setFont(boldFont)
+
+        self.button_Dirichlet_ = QtGui.QPushButton('Apply Dirichlet B.C.',self)
+        self.button_Dirichlet_.clicked.connect(self.onDirichletBC)
+        self.button_Dirichlet_.setAutoDefault(False)
+        self.button_Dirichlet_.move(10, 555)
+
+        self.button_Neumann_ = QtGui.QPushButton('Apply Neumann B.C.',self)
+        self.button_Neumann_.clicked.connect(self.onNeumannBC)
+        self.button_Neumann_.setAutoDefault(False)
+        self.button_Neumann_.move(180, 555)
+
         # cancel button
         cancelButton = QtGui.QPushButton('Cancel', self)
         cancelButton.clicked.connect(self.onCancel)
-        cancelButton.move(200, 550)
+        cancelButton.move(200, 650)
         # OK button
         okButton = QtGui.QPushButton('OK', self)
         okButton.clicked.connect(self.onOk)
         okButton.setAutoDefault(True)
-        okButton.move(80, 550)
+        okButton.move(80, 650)
+
+        # show the dialog box
+        self.DirichletBCBox_flag = False
+        self.DirichletBCBox_Fun()
+        self.show()
+
+                            ############################# FUNCTION DEFINITIONS #############################
 
     def onBrowseButton(self):
         self.browseWindow = QtGui.QFileDialog(self)
@@ -180,7 +218,131 @@ class TibraParameters(QtGui.QDialog):
         if self.browseWindow.exec_():
             path_name_list = self.browseWindow.selectedFiles()
             self.textInput_pathname_.setText(path_name_list[0])
-    
+
+
+    def onDirichletBC(self):
+        self.DirichletDialogBox_Fun()
+        self.DirichletDialogBox.exec_()
+
+    def DirichletDialogBox_Fun(self):
+
+        self.DirichletDialogBox = QtGui.QDialog(self)
+        width = 470
+        height = 100
+        centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
+        self.DirichletDialogBox.setGeometry(centerPoint.x()-0.5*width, centerPoint.y()-0.5*height, width, height)
+        self.DirichletDialogBox.setWindowTitle("Apply Dirichlet Boundary Condition")
+
+        self.DirichletDialogBox.label_dirichlet = QtGui.QLabel("Please enter the number faces subject to Dirichlet BC:", self.DirichletDialogBox)
+        self.DirichletDialogBox.label_dirichlet.move(10, 30)
+
+        self.DirichletDialogBox.DirichletFaceNumber = QtGui.QLineEdit(self.DirichletDialogBox)
+        self.DirichletDialogBox.DirichletFaceNumber.setPlaceholderText("example: 3")
+        self.DirichletDialogBox.DirichletFaceNumber.setFixedWidth(80)
+        self.DirichletDialogBox.DirichletFaceNumber.move(380, 28)
+
+        okButton_DirichletDialogBox = QtGui.QPushButton('OK', self.DirichletDialogBox)
+        okButton_DirichletDialogBox.move(0.5*width - 15, 65)
+        okButton_DirichletDialogBox.clicked.connect(self.okButton_DirichletDialogBox)
+        okButton_DirichletDialogBox.setAutoDefault(True)
+        
+
+    def okButton_DirichletDialogBox(self):
+        self.dirichlet_faces= self.DirichletDialogBox.DirichletFaceNumber.text()
+        infoBox = QtGui.QMessageBox.information(self.DirichletDialogBox, "Apply Dirichlet Boundary Conditions", \
+                                                "Please select " + self.dirichlet_faces + \
+                                                " faces subject to Dirichlet BC one by one!")
+        
+        if infoBox == QtGui.QMessageBox.StandardButton.Ok:
+            self.view = Gui.ActiveDocument.ActiveView
+            self.callback = self.view.addEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.getMouseClick_DirichletBCBox)
+            self.DirichletDialogBox.close()
+            self.setVisible(False)
+
+
+    def getMouseClick_DirichletBCBox(self, event_cb):   
+        event = event_cb.getEvent()
+        if (coin.SoMouseButtonEvent.isButtonPressEvent(event, coin.SoMouseButtonEvent.BUTTON1) == True) & (Gui.Selection.hasSelection() == True):
+            if (self.dirichlet_count <= int(self.dirichlet_faces)):
+                    self.DirichletBCBox_Fun()               
+                    self.DirichletBCBox.exec_()
+                    if(self.dirichlet_count > int(self.dirichlet_faces)):
+                        self.setVisible(True)
+                        self.view.removeEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.callback)
+                        
+
+    def DirichletBCBox_Fun(self):
+            self.DirichletBCBox = QtGui.QDialog(self)
+            width = 350
+            height = 120
+            centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
+            self.DirichletBCBox.setGeometry(centerPoint.x()-0.5*width, centerPoint.y()-0.5*height, width, height)
+            self.DirichletBCBox.setWindowTitle("Apply Dirichlet Boundary Condition")
+            self.DirichletBCBox.label_dirichlet = QtGui.QLabel("Please enter the displacement constraint values:", self.DirichletBCBox)
+            self.DirichletBCBox.label_dirichlet.move(10, 20)
+
+            self.DirichletBCBox.label_x_constraint = QtGui.QLabel("x: ", self.DirichletBCBox)
+            self.DirichletBCBox.label_x_constraint.move(10,48)
+            self.DirichletBCBox.text_x_constraint = QtGui.QLineEdit(self.DirichletBCBox)
+            self.DirichletBCBox.text_x_constraint.setFixedWidth(80)
+            self.DirichletBCBox.text_x_constraint.move(30, 45)
+
+            self.DirichletBCBox.label_y_constraint = QtGui.QLabel("y: ", self.DirichletBCBox)
+            self.DirichletBCBox.label_y_constraint.move(120,48)
+            self.DirichletBCBox.text_y_constraint = QtGui.QLineEdit(self.DirichletBCBox)
+            self.DirichletBCBox.text_y_constraint.setFixedWidth(80)
+            self.DirichletBCBox.text_y_constraint.move(140, 45)
+
+            self.DirichletBCBox.label_z_constraint = QtGui.QLabel("z: ", self.DirichletBCBox)
+            self.DirichletBCBox.label_z_constraint.move(230, 48)
+            self.DirichletBCBox.text_z_constraint = QtGui.QLineEdit(self.DirichletBCBox)
+            self.DirichletBCBox.text_z_constraint.setFixedWidth(80)
+            self.DirichletBCBox.text_z_constraint.move(250, 45)
+
+            okButton_DirichletBCBox = QtGui.QPushButton('OK', self.DirichletBCBox)
+            okButton_DirichletBCBox.move(140, 85)
+            okButton_DirichletBCBox.clicked.connect(self.okButton_DirichletBCBox)
+            okButton_DirichletBCBox.setAutoDefault(True)
+
+    def okButton_DirichletBCBox(self):
+            print("Mouse Click " + str(self.dirichlet_count))
+            self.dirichlet_count = self.dirichlet_count + 1
+            Gui.Selection.clearSelection()
+            self.DirichletBCBox.close()
+
+
+    def onNeumannBC(self):
+        self.NeumannDialogBox = QtGui.QDialog(self)
+        width = 470
+        height = 100
+        self.NeumannDialogBox.setGeometry(self.centerPoint.x()-0.5*width, self.centerPoint.y()-0.5*height, width, height)
+        self.NeumannDialogBox.setWindowTitle("Apply Neumann Boundary Condition")
+
+        self.NeumannDialogBox.label_Neumann = QtGui.QLabel("Please enter the number faces subject to Neumann BC:", self.NeumannDialogBox)
+        self.NeumannDialogBox.label_Neumann.move(10, 30)
+
+        self.NeumannDialogBox.NeumannFaceNumber = QtGui.QLineEdit(self.NeumannDialogBox)
+        self.NeumannDialogBox.NeumannFaceNumber.setPlaceholderText("example: 3")
+        self.NeumannDialogBox.NeumannFaceNumber.setFixedWidth(80)
+        self.NeumannDialogBox.NeumannFaceNumber.move(380, 28)
+
+        okButton_Neumann = QtGui.QPushButton('OK', self.NeumannDialogBox)
+        okButton_Neumann.move(0.5*width - 15, 65)
+        okButton_Neumann.clicked.connect(self.okButton_Neumann)
+        okButton_Neumann.setAutoDefault(True)
+        self.NeumannDialogBox.exec_()
+
+    def okButton_Neumann(self):
+        self.Neumann_faces = self.NeumannDialogBox.NeumannFaceNumber.text()
+        infoBox = QtGui.QMessageBox.information(self.NeumannDialogBox, "Apply Neumann Boundary Conditions", \
+                                                "Please select " + self.Neumann_faces + \
+                                                " faces subject to Neumann BC one by one!")
+        if infoBox == QtGui.QMessageBox.StandardButton.Ok:
+            self.NeumannDialogBox.close()
+            self.setVisible(False)
+
+
+
     def onOk(self):
         #bounds
         mybounds=self.bounds()
